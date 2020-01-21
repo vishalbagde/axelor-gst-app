967d63047b8060f6d62c523f09abf6fffe0b2984
vishalbagde <vishalbagde.mmm@gmail.com>
2020-01-21 18:43:18 +0530

    add gst calculation method in invoice

--- modules/axelor-gst/src/main/java/com/axelor/gst/db/web/InvoiceController.java
+++ modules/axelor-gst/src/main/java/com/axelor/gst/db/web/InvoiceController.java
@@ -3,56 +3,82 @@ package com.axelor.gst.db.web;
 import com.axelor.gst.db.Address;
 import com.axelor.gst.db.Contact;
 import com.axelor.gst.db.Invoice;
+import com.axelor.gst.db.InvoiceLine;
 import com.axelor.gst.db.Party;
 import com.axelor.gst.db.repo.InvoiceRepo;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
 import com.google.inject.Inject;
+
+import java.math.BigDecimal;
+
 import java.util.List;
 
 public class InvoiceController {
-  @Inject InvoiceRepo invoiceRepo;
+	@Inject
+	InvoiceRepo invoiceRepo;
 
-  public void setDefaultCompanyInInvoice(ActionRequest request, ActionResponse response) {
-    Invoice invoice = request.getContext().asType(Invoice.class);
-    // invoiceRepo.all().filter("self.company", params)
-  }
+	public void setDefaultCompanyInInvoice(ActionRequest request, ActionResponse response) {
+		//Invoice invoice = request.getContext().asType(Invoice.class);
+		// invoiceRepo.all().filter("self.company", params)
+	}
 
-  public void setPrimaryContactAndAddressInInvoice(ActionRequest request, ActionResponse response) {
-    Invoice invoice = request.getContext().asType(Invoice.class);
-    Party party = invoice.getParty();
-    try {
+	public void setPrimaryContactAndAddressInInvoice(ActionRequest request, ActionResponse response) {
+		Invoice invoice = request.getContext().asType(Invoice.class);
+		Party party = invoice.getParty();
+		try {
 
-      if (party != null) {
-        List<Contact> contactList = party.getContactList();
-        if (!contactList.isEmpty() || contactList == null) {
-          for (Contact contact : contactList) {
-            if (contact.getTypeSelect().equals("primary")) {
-              response.setValue("partyContact", contact);
-              break;
-            }
-          }
-          List<Address> addressList = party.getAddressList();
-          for (Address address : addressList) {
-            if (address.getTypeSelect().equals("default")
-                || address.getTypeSelect().equals("invoice")) {
-              response.setValue("invoiceAddress", address);
-              break;
-            }
-          }
-          if (!invoice.getIsInvoiceAddAsShippingAdd()) {
-            for (Address address : addressList) {
-              if (address.getTypeSelect().equals("default")
-                  || address.getTypeSelect().equals("shipping")) {
-                response.setValue("shippingAddress", address);
-                break;
-              }
-            }
-          }
-        }
-      }
-    } catch (Exception e) {
-      response.setFlash(e.fillInStackTrace().toString());
-    }
-  }
+			if (party != null) {
+				List<Contact> contactList = party.getContactList();
+				if (!contactList.isEmpty() || contactList == null) {
+					for (Contact contact : contactList) {
+						if (contact.getTypeSelect().equals("primary")) {
+							response.setValue("partyContact", contact);
+							break;
+						}
+					}
+					List<Address> addressList = party.getAddressList();
+					for (Address address : addressList) {
+						if (address.getTypeSelect().equals("default") || address.getTypeSelect().equals("invoice")) {
+							response.setValue("invoiceAddress", address);
+							break;
+						}
+					}
+					if (!invoice.getIsInvoiceAddAsShippingAdd()) {
+						for (Address address : addressList) {
+							if (address.getTypeSelect().equals("default")
+									|| address.getTypeSelect().equals("shipping")) {
+								response.setValue("shippingAddress", address);
+								break;
+							}
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			response.setFlash(e.fillInStackTrace().toString());
+		}
+	}
+
+	public void setTotalInInvoice(ActionRequest request, ActionResponse response) {
+		Invoice invoice = request.getContext().asType(Invoice.class);
+		List<InvoiceLine> invoiceLineLis = invoice.getInvoiceLineList();
+		float cgst = 0, sgst = 0, igst=0, netTotal = 0, grossTotal = 0;
+
+		
+		for (InvoiceLine invoiceLine : invoiceLineLis) {
+			
+			  netTotal+=invoiceLine.getNetAmount().floatValue();
+			  sgst+=invoiceLine.getSgst().floatValue();
+			  cgst+=invoiceLine.getCgst().floatValue();
+			  igst+=invoiceLine.getIgst().floatValue();
+			  grossTotal+=invoiceLine.getGrossAmount().floatValue();	  
+		}
+		response.setValue("igst", BigDecimal.valueOf(igst));
+		response.setValue("cgst", BigDecimal.valueOf(cgst));
+		response.setValue("sgst", BigDecimal.valueOf(sgst));
+		response.setValue("netAmount", BigDecimal.valueOf(netTotal));
+		response.setValue("grossAmount", BigDecimal.valueOf(grossTotal));
+
+	}
 }
--- modules/axelor-gst/src/main/java/com/axelor/gst/db/web/InvoiceLineController.java
+++ modules/axelor-gst/src/main/java/com/axelor/gst/db/web/InvoiceLineController.java
@@ -1,17 +1,40 @@
 package com.axelor.gst.db.web;
 
+import java.math.BigDecimal;
+
+import com.axelor.gst.db.Address;
 import com.axelor.gst.db.Invoice;
 import com.axelor.gst.db.InvoiceLine;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
 
+
 public class InvoiceLineController {
 
   public void calculateGst(ActionRequest request, ActionResponse response) {
     InvoiceLine invoiceLine = request.getContext().asType(InvoiceLine.class);
-     response.setFlash(invoiceLine.getHsbn().toString());
-     Invoice invoice = invoiceLine.getInvoice();
-     response.setFlash(invoice.getInvoiceDateT().toString());
+    Invoice invoice = request.getContext().getParentContext().asType(Invoice.class);
+
+    Address companyAddress = invoice.getCompany().getAddress();
+    Address partyAddress = invoice.getInvoiceAddress();
+
+    float gstAmount = 0;
+    gstAmount =
+        invoiceLine.getNetAmount().floatValue() * invoiceLine.getGstRate().floatValue() / 100;
+    if (companyAddress.getState().equals(partyAddress.getState())) {
+      response.setValue("sgst", BigDecimal.valueOf(gstAmount / 2));
+      invoiceLine.setSgst(BigDecimal.valueOf(gstAmount / 2));
+      response.setValue("cgst", BigDecimal.valueOf(gstAmount / 2));
+      invoiceLine.setCgst(BigDecimal.valueOf(gstAmount / 2));
+    } else {
+      response.setValue("igst", BigDecimal.valueOf(gstAmount));
+      invoiceLine.setIgst(BigDecimal.valueOf(gstAmount));
+    }
+
+    response.setValue("grossAmount", invoiceLine.getNetAmount().floatValue() + gstAmount);
+    invoiceLine.setGrossAmount(BigDecimal.valueOf(invoiceLine.getNetAmount().floatValue() + gstAmount));
+    invoice.setIgst(BigDecimal.valueOf(gstAmount));
+    response.setValues(invoiceLine);
     
   }
 }
